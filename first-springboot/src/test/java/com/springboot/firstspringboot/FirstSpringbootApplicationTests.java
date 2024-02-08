ff20dcbe96bb769f17083d96c4412cbe99e1cbf4
Brian Lapuste <67889855+LapusteB@users.noreply.github.com>
2024-02-08 15:22:02 -0500

    Init blank start.spring.io

---
+++ first-springboot/src/test/java/com/springboot/firstspringboot/FirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
